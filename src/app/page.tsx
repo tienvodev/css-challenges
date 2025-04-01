94dec70890b81dfb8da16c361321b7b020c244b4
Tien Vo <tienhoangvo.dev@gmail.com>
2025-04-01 20:40:48 +0700

    fix: wording update wording for links in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import styles from "./HomePage.module.css";
 export default function HomePage() {
   return (
     <body className={styles.body}>
-      {/* A list of links to challenges */}
       <nav className={styles.nav}>
         <Link href="/challenges/challenge-01" className={styles.link}>
           Challenge 01: Responsive Grid of Squares | Style Siblings on Hover
